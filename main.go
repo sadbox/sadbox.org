749915fb8c45e00da5c27bcafe9f14adbde888ef
James McGuire <james@sadbox.org>
2017-03-21 16:14:27 -0700

    Use all sorts of headers, disable inline css, etc

--- main.go
+++ main.go
@@ -123,6 +123,11 @@ func AddHeaders(handler http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Cache-Control", "max-age=120")
 		w.Header().Set("Strict-Transport-Security", "max-age=31536000; includeSubDomains; preload")
+		w.Header().Set("Content-Security-Policy", "default-src 'self'; object-src 'none'; frame-ancestors 'none'")
+		w.Header().Set("X-Frame-Options", "DENY")
+		w.Header().Set("X-Content-Type-Options", "nosniff")
+		w.Header().Set("Referrer-Policy", "same-origin")
+		w.Header().Set("X-XSS-Protection", "1; mode=block")
 		handler.ServeHTTP(w, r)
 	})
 }
